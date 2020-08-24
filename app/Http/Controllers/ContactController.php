3de77a71253c28b9589137c0aedc31de13c3363c
hebazo <48584444+hebazo@users.noreply.github.com>
2020-08-24 16:53:11 +0200

    html email using mailable classes

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ContactMe;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -14,10 +15,7 @@ class ContactController extends Controller
     public function store(){
         request()->validate(['email' => 'required|email']);
 
-        Mail::raw('It works', function ($message){
-            $message->to(request('email'))->subject('hello there');
-        });
-
-        return redirect('/contact')->with('message', 'email sent!');
+        Mail::to(request('email'))->send(new ContactMe('shirts'));
+       return redirect('/contact')->with('message', 'email sent!');
     }
 }

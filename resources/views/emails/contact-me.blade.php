75dfe23b3689ee60b4b189e5a8b59e471192d30c
hebazo <48584444+hebazo@users.noreply.github.com>
2020-08-24 17:07:50 +0200

    markdown templates

--- resources/views/emails/contact-me.blade.php
+++ resources/views/emails/contact-me.blade.php
@@ -1,15 +1,15 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>it works again!</h1>
+@component('Mail::message')
+    # A Heading
 
-<p> blah blah blah {{ $topic }}</p>
-</body>
-</html>
+    skdifsjdfhjsdkjf
+
+    - A list
+    - goes
+    - here
+
+    @component('mail::button', ['url' => 'google.com'])
+        visit labalah
+
+        @endcomponent
+
+@endcomponent
